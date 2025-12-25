52c82f8ab14a021eda280ff63d7c154752b3b639
honey766 <honey76684@gmail.com>
2025-12-25 19:39:49 +0900

    데스크톱에서 버전 표시 위치 변경
    데스크톱에서는 버전 숫자가 오른쪽 아래에 표시되도록 수정

--- Assets/Scripts/Main/GetGameVersion.cs
+++ Assets/Scripts/Main/GetGameVersion.cs
@@ -5,7 +5,21 @@ public class GetGameVersion : MonoBehaviour
 {
     void Start()
     {
+        TextMeshProUGUI tmpro = GetComponent<TextMeshProUGUI>();
+        RectTransform rect = GetComponent<RectTransform>();
         string gameVersion = Application.version;
-        GetComponent<TextMeshProUGUI>().text = gameVersion;
+        tmpro.text = gameVersion;
+
+        #if UNITY_STANDALONE
+        tmpro.alignment = TextAlignmentOptions.BottomRight;
+        rect.anchorMin = new Vector2(1f, 0f);
+        rect.anchorMax = new Vector2(1f, 0f);
+        rect.pivot = new Vector2(1f, 0f);
+        #else
+        tmpro.alignment = TextAlignmentOptions.Bottom;
+        rect.anchorMin = new Vector2(0.5f, 0f);
+        rect.anchorMax = new Vector2(0.5f, 0f);
+        rect.pivot = new Vector2(0.5f, 0f);
+        #endif
     }
 }
